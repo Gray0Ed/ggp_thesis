7e9ad518fac1948112df244f811084fcb4056652
Jan Kwasniak <kwasniakjanek@gmail.com>
2016-09-20 16:08:39 +0200

    most of recompressor ready for testing

--- rule_engine/recompressor.cpp
+++ rule_engine/recompressor.cpp
@@ -2,6 +2,18 @@
 #include <fstream>
 #include <unordered_map>
 
+
+#ifndef NO_BACKWARD
+#define BACKWARD_HAS_DW 1
+#include "backward.hpp"
+
+namespace backward {
+    backward::SignalHandling sh;
+};
+
+#endif
+
+
 using namespace std;
 #include "common.hpp"
 
@@ -22,22 +34,23 @@ namespace SENTENCE_TYPE {
     }
 };
 
-struct ForwardList {
+struct TheoremCounters {
     int sentence_id;
     short counter_max;
     short counter_value;
     bool bad, always_false;
-    ForwardList(int _sentence_id, short _counter_max, bool _always_false) {
+    TheoremCounters(int _sentence_id, short _counter_max, bool _always_false) {
         sentence_id = _sentence_id;
         counter_max = _counter_max;
         always_false = _always_false;
         bad = false;
-        counter_value = false;
+        counter_value = 0;
     }
 };
 
-struct BootstrapForwardPropData {
-    vector<ForwardList> counters;
+struct BootstrapPropData {
+    vector<TheoremCounters> counters;
+    vector<vector<int>> forward_deps;
     vector<vector<int>> dependencies;
     vecotr<int> types;
     vector<int> theorems_n;
@@ -58,6 +71,7 @@ template <typename T> int sgn(T val) {
 }
 
 unordered_map<string, int> sentence_ids;
+vector<string> sentence_id_to_str;
 unordered_map<int, int> value_to_theo_type;
 
 void prepare_value_to_theo_type_map() {
@@ -108,41 +122,69 @@ void generate_sentence_ids(const string &in_filename) {
     }
     checked_sentences.clear();
     int id_counter = 1;
+    sentence_id_to_str.push_back("$NOPE$!!!");
     for (int theo_type = 0; theo_type < N_SENTENCE_TYPES; ++theo_type) {
         const auto &sentence_strs = by_theo_type[theo_type];
         for (const auto &sentence: sentence_strs) {
             sentence_ids[sentence] = id_counter++;
+            sentence_id_to_str.push_back(sentence);
+            assert(sentence_id_to_str[id_counter - 1] == sentence);
         }
     }
 }
 
+namespace SUB_SENTENCE_FLAG  {
+    enum {
+        NORMAL,
+        NEGATED,
+        ALWAYS_FALSE,
+        ALWAYS_TRUE,
+    };
+};
 
-// returns -id if negated
-// distinct == 1 if normal distinct
-// distinct == -1 if negated distinct
-int get_sub_sentence_smart_id(const HighNode &snode, int &distinct) {
+int get_sub_sentence_id(const HighNode &snode, int &flag) {
     distinct = 0;
     if (snode.value == DISTINCT_TOKEN_ID) {
-        distinct = 1;
+        flag = SUB_SENTENCE_FLAG::ALWAYS_TRUE;
         return 0;
     }
     if (snode.value == NOT_TOKEN_ID) {
         assert(snode.sub.size() > 0);
-        int ret = -get_sub_sentence_smart_id(snode.sub[0], distinct);
-        distinct = -distinct;
+        int ret = get_sub_sentence_id(snode.sub[0], flag);
+        if (flag == SUB_SENTENCE_FLAG::NEGATED) {
+            flag = SUB_SENTENCE_FLAG::NORMAL;
+        } else if (flag == SUB_SENTENCE_FLAG::NORMAL) {
+            flag = SUB_SENTENCE_FLAG::NEGATED;
+        } else if (flag == SUB_SENTENCE_FLAG::ALWAYS_FALSE) {
+            flag = SUB_SENTENCE_FLAG::ALWAYS_TRUE;
+        } else if (flag == SUB_SENTENCE_FLAG::ALWAYS_TRUE) {
+            flag = SUB_SENTENCE_FLAG::ALWAYS_FALSE;
+        } else {
+            assert(false);
+        }
         return ret;
     }
-    return sentences_ids[snode.to_string()];
+    const string snode_string = snode.to_string();
+    if (sentences_ids.count(snode_string) == 0) {
+        flag = SUB_SENTENCE_FLAG::ALWAYS_FALSE;
+        return 0;
+    } else {
+        flag = SUB_SENTENCE_FLAG::NORMAL;
+        return sentences_ids[snode.to_string()];
+    }
 }
 
-void load_bootstrap_forward_prop_data(const string &in_filename, 
-                                      BootstrapForwardPropData &to_fill) {
+void load_bootstrap_prop_data(const string &in_filename, 
+                              BootstrapPropData &to_fill,
+                              vector<bool> &const_sentences) {
     // remove distinct (bc, always true)
     const int max_sentence_id = sentence_ids.size();
     to_fill.counters.resize(0);
-    to_fill.dependencies.resize(max_sentence_id + 1);
+    to_fill.counters.push_back(TheoremCounters(0, 0, true)); // dummy theorem 0
+    to_fill.dependencies.resize(0);
+    to_fill.dependencies.push_back(vector<int>());
+    to_fill.forward_deps.resize(max_sentence_id + 1);
     to_fill.types.resize(max_sentence_id + 1);
-    to_fill.counters.push_back(ForwardList(0, 0, true)); // dummy theorem 0
     to_fill.theorems_n.resize(0);
     to_fill.theorems_n.resize(max_sentence_id + 1);
     ifstream inpf(in_filename);
@@ -159,48 +201,51 @@ void load_bootstrap_forward_prop_data(const string &in_filename,
         ++to_fill.theorems_n[head_id];
         bool always_false = false;
         int forward_counter = 0;
+        to_fill.dependencies.push_back(vector<int>());
         for (int i = 1; i < node.sub.size(); ++i) {
             const auto &snode = node.sub[i];
             assert(snode.value != NEXT_TOKEN_ID);
             assert(snode.value != LEGAL_TOKEN_ID);
-            int distinct;
-            const int sub_sentence_id = get_sub_sentence_id(snode, distinct);
-            if (sub_sentence_id == 0) {
-                assert(distinct == 1 || distinct == -1);
-                if (distinct == -1) { // this theorem is always false
-                    always_false = true;
-                    break;
-                } else {
-                    assert(distinct == 1);
-                    continue; // skip sentence, because it is always true
+            int flag;
+            const int sub_sentence_id = get_sub_sentence_id(snode, flag);
+            if (flag == SUB_SENTENCE_FLAG::ALWAYS_TRUE) {
+                continue; // skip sentence, because it is always true
+            } else if (flag == SUB_SENTENCE_FLAG::ALWAYS_FALSE) {
+                always_false = true;
+                break;
+            }  else {
+                assert(flag == SUB_SENTENCE_FLAG::NORMAL ||
+                       flag == SUB_SENTENCE_FLAG::NEGATED);
+                int mult = 1;
+                if (flag == SUB_SENTENCE_FLAG::NEGATED) {
+                    mult = -1;
                 }
-            } else {
                 ++forward_counter;
-                to_fill.dependencies[abs(sub_sentence_id)].push_back(
-                        sgn(sub_sentence_id) * theorem_id);
+                to_fill.forward_deps[sub_sentence_id].push_back(mult * theorem_id);
+                to_fill.dependencies.back().push_back(mult * sub_sentence_id);
             }
         }
-        to_fill.counters.push_back(ForwardList(head_id, forward_counter, always_false));
+        to_fill.counters.push_back(TheoremCounters(head_id, forward_counter, always_false));
         ++theorem_id;
         assert(to_fill.counters.size() == theorem_id);
     }
 }
 
 
-void find_ids_to_remove(BootstrapForwardPropData &bpfd, 
-        vector<bool> &theo_ids_to_remove, vector<bool> &sentence_ids_to_remove) {
+void find_ids_to_remove(BootstrapPropData &bpfd, 
+        vector<bool> &const_theos, vector<bool> &const_sentences) {
     vector<pair<int, bool>> const_sentences_queue;
     int theorem_id = 1;
 
     debug_always_true_sentence.resize(0);
-    debug_always_true_sentence.resize(bfpd.types.size())
+    debug_always_true_sentence.resize(bpd.types.size())
     debug_always_false_sentence.resize(0);
-    debug_always_false_sentence.resize(bfpd.types.size());
+    debug_always_false_sentence.resize(bpd.types.size());
 
     debug_always_true_theorem.resize(0);
-    debug_always_true_theorem.resize(bfpd.counters.size());
+    debug_always_true_theorem.resize(bpd.counters.size());
     debug_always_false_theorem.resize(0);
-    debug_always_false_theorem.resize(bfpd.counters.size());
+    debug_always_false_theorem.resize(bpd.counters.size());
 
     for (const auto &forward: bpfd.counters) {
         int sentence_to_append = 0;
@@ -218,22 +263,22 @@ void find_ids_to_remove(BootstrapForwardPropData &bpfd,
                bpdf.types[forward.sentence_id] != SENTENCE_TYPE::DOES);
 
         if (forward.always_false) {
-            theo_ids_to_remove[theorem_id] = true;
+            const_theos[theorem_id] = true;
             debug_always_false_theorem[theorem_id] = true;
-            assert(bfpd.theorems_n[forward.sentence_id] > 0);
-            --bfpd.theorems_n[forward.sentence_id];
-            if (bfpd.theorems_n[forward.sentence_id] == 0) {
-                sentence_ids_to_remove[forward.sentence_id] = true;
+            assert(bpd.theorems_n[forward.sentence_id] > 0);
+            --bpd.theorems_n[forward.sentence_id];
+            if (bpd.theorems_n[forward.sentence_id] == 0) {
+                const_sentences[forward.sentence_id] = true;
                 debug_always_false_sentence[forward.sentence_id] = true;
                 const_sentences_queue.push_back(make_pair(forward.sentence_id, false));
             }
         }
         if (forward.counter_max == 0) {
-            theo_ids_to_remove[theorem_id] = true;
+            const_theos[theorem_id] = true;
             debug_always_true_theorem[theorem_id] = true;
             debug_always_true_sentence[forward.sentence_id] = true;
-            if (sentence_ids_to_remove[forward.sentence_id] == false) {
-                sentence_ids_to_remove[forward.sentence_id] = true;
+            if (const_sentences[forward.sentence_id] == false) {
+                const_sentences[forward.sentence_id] = true;
                 const_sentences_queue.push_back(make_pair(forward.sentence_id, true));
             }
         }
@@ -244,24 +289,101 @@ void find_ids_to_remove(BootstrapForwardPropData &bpfd,
         const_sentences_queue.pop_back();
         const int sentence_id = to_process.first;
         const bool sentence_value = to_process.second;
-        
-        :qa
-        :qa
+        const auto &deps = bpd.forward_deps[sentence_id];
+        for (int theo_id: deps) {
+            if (!sentence_value) {
+                theo_id = -theo_id;
+            }
+            if (theo_id > 0) {
+                auto &tcounter = bpd.counters[theo_id];
+                ++tcounter.counter_value;
+                assert(tcounter.counter_value <= tcounter.counter_max);
+                if (tcounter.counter_value == tcounter.counter_max) {
+                    const_theos[theo_id] = true;
+                    assert(!const_sentences[tcounter.sentence_id]);
+                    const_sentences[tcounteer.sentence_id] = true;
+                    debug_always_true_theorem[theo_id] = true;
+                    debug_always_true_sentence[tcounter.sentence_id] = true;
+                    const_sentences_queue.push_back(make_pair(tcounter.sentence_id, true));
+                }
+            } else {
+                assert(theo_id < 0);
+                theo_id = -theo_id;
+                int dep_sentence_id = bpd.counters[theo_id].sentence_id;
+                debug_always_false_theorem[theo_id] = true;
+                const_theos[theo_id] = true;
+                --bpd.theorems_n[dep_sentence_id];
+                assert(bpd.theorems_n[dep_sentence_id] >= 0);
+                if (bpd.theorems_n[dep_sentence_id] == 0) {
+                    debug_always_false_sentence[dep_sentence_id] = true;
+                    assert(!const_sentences[dep_sentence_id]);
+                    const_sentences[dep_sentence_id] = true;
+                    const_sentences_queue.push_back(make_pair(dep_sentence_id, false));
+                }
+            }
+        }
     }
 }
 
+string theorem_to_string(int theorem_id, const BootstrapPropData &bpd) {
+    string res = "( <= ( ";
+    res = sentence_id_to_str[bpd.counters[theorem_id].sentence_id] + " )";
+    for (int deps: bpd.dependencies) {
+        int sentence_id = deps;
+        res += "( ";
+        if (deps < 0) {
+            res += " not ( ";
+            sentence_id *= -1;
+        }
+        res += sentence_id_to_str[sentence_id] + " )";
+        if (deps < 0) {
+            res += " )";
+        }
+    }
+    return res;
+}
 
 void remove_const_sentences(const string &in_filename) {
-    // remove normal and next which are always true, assert there is no always true does
-    // if there is always true legal, terminal, goal, init or next- leave it
+    // remove const normal which, assert there is no always true does
+    // if there is const legal, terminal, goal, init or next- leave it
     // remove distinct (because it is always true)
-    BootstrapForwardPropData bfpd;
-    load_bootstrap_forward_prop_data(in_filename, bfpd);
-    vector<bool> theo_ids_to_remove;
-    vector<bool> sentence_ids_to_remove;
-    theo_ids_to_remove.resize(bfpd.counters.size());
-    sentence_ids_to_remove.resize(bfpd.types.size());
-    find_ids_to_remove(bfpd, theo_ids_to_remove, sentence_ids_to_remove);
+    BootstrapPropData bpd;
+    load_bootstrap_prop_data(in_filename, bpd);
+    vector<bool> const_theos;
+    vector<bool> const_sentences;
+    const_theos.resize(bpd.counters.size());
+    const_sentences.resize(bpd.types.size());
+    find_ids_to_remove(bpd, const_theos, const_sentences);
+    for (int i = 1; i < debug_always_true_sentence.size(); ++i) {
+        assert((debug_always_true_sentence[i] || debug_always_false_sentence[i]) ==
+               const_sentences[i]);
+    }
+    for (int i = 1; i < debug_always_true_theorem.size(); ++i) {
+        assert((debug_always_true_theorem[i] || debug_always_false_theorem[i]) ==
+               const_theos[i]);
+    }
+    cerr << "CONST SENTENCES:\n";
+    for (int i = 1; i < const_sentences.size(); ++i) if (const_sentences[i]) {
+        cerr << "always ";
+        if (debug_always_true_sentence[i]) {
+            cerr << "true: ";
+        } else {
+            assert(debug_always_false_sentence[i]);
+            cerr << "false: ";
+        }
+        cerr << sentence_id_to_str[i] << "\n";
+    }
+    cerr << "CONST THEOREMS:\n";
+    for (int i = 1; i < const_theos.size(); ++i) if (const_theos[i]) {
+        cerr << "always ";
+        if (debug_always_true_theorem[i]) {
+            cerr << "true: ";
+        } else {
+            assert(debug_always_false_theorem[i]);
+            cerr << "false: ";
+        }
+        cerr << theorem_to_string(i) << "\n";
+    }
 }
 
 int main(int argc, char **argv) {
@@ -270,8 +392,9 @@ int main(int argc, char **argv) {
         return 1;
     }
     generate_sentence_ids(argv[1]);
+    remove_const_sentences(argv[1]);
 
-    // filter out always true
+    // filter out const sentences and theorems
     // split by:
     // * does (input)
     // * true (input)
